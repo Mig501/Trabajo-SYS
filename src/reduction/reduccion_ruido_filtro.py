6f6e825a2a81c766e4811da3f5f7251a005533ac
blooraayy <jaime.alv.fdez@gmail.com>
2026-01-08 13:51:06 +0100

    c

--- src/reduction/reduccion_ruido_filtro.py
+++ src/reduction/reduccion_ruido_filtro.py
@@ -8,7 +8,15 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from audio_loader import load_audio
 
 # Audios de ejemplo (uno por tipo)
-AUDIO_FILES = {
+AUDIO_FILES_1 = {
+    "exterior": "1-18527-B-44.wav",
+    "persona": "3-103599-B-25.wav",
+    "animal": "5-9032-A-0.wav",
+    "interior": "1-12654-B-15.wav",
+    "natural": "5-219379-A-11.wav"
+}
+
+AUDIO_FILES_2 = {
     "exterior": "1-18527-B-44.wav",
     "persona": "3-103599-B-25.wav",
     "animal": "5-9032-A-0.wav",
@@ -28,7 +36,37 @@ fig, axes = plt.subplots(5, 2, figsize=(14, 12))
 fig.suptitle("Filtrado en frecuencia: señal original vs filtrada", fontsize=16)
 
 row = 0
-for clase, file in AUDIO_FILES.items():
+for clase, file in AUDIO_FILES_1.items():
+    x, sr = load_audio(os.path.join(BASE_PATH, file))
+
+    # Filtrado
+    x_low = butter_filter(x, sr, cutoff=1000, btype="low")
+    x_high = butter_filter(x, sr, cutoff=500, btype="high")
+
+    t = np.arange(len(x)) / sr
+
+    # Señal original
+    axes[row, 0].plot(t, x)
+    axes[row, 0].set_title(f"{clase} | Original")
+    axes[row, 0].set_ylabel("Amplitud")
+    axes[row, 0].grid(True)
+
+    # Señal filtrada
+    axes[row, 1].plot(t, x_low, label="Pasa-bajo")
+    axes[row, 1].plot(t, x_high, label="Pasa-alto", alpha=0.7)
+    axes[row, 1].set_title(f"{clase} | Filtrada")
+    axes[row, 1].legend()
+    axes[row, 1].grid(True)
+
+    row += 1
+
+for ax in axes[-1, :]:
+    ax.set_xlabel("Tiempo (s)")
+
+plt.tight_layout(rect=[0, 0, 1, 0.95])
+plt.show()
+
+for clase, file in AUDIO_FILES_2.items():
     x, sr = load_audio(os.path.join(BASE_PATH, file))
 
     # Filtrado
